45cf797a19b173b51319adb64b199ae0eecb0641
andrexa1337 <166932134+andrexa1337@users.noreply.github.com>
2024-04-13 19:04:13 +0400

    Add files via upload

---
+++ day4/homework/homework.py
@@ -0,0 +1,5 @@
+name = "aleqsandre"
+lastname = "kupatadze"
+age = 12
+age_in_10_years = age + 10
+print("user name is " +  name  + " user lastname is " + lastname + " user age in 10 years " + str(age_in_10_years) )
