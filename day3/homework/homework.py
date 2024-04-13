ae10f88b8e16e484c3ea976195f384920075c954
andrexa1337 <166932134+andrexa1337@users.noreply.github.com>
2024-04-13 19:03:24 +0400

    Add files via upload

---
+++ day3/homework/homework.py
@@ -0,0 +1,7 @@
+name1 = "vanda"
+name2 = "lasha"
+name3 = "lia"
+name4 = "gia"
+name5 = "salome"
+name6 = "aleqsandre"
+print(name1 + " " +name2 + " " +name3 + " " +name4 + " " + name5 + " " + name6)
