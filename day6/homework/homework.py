cc002fc590e694b5b65604725b13b8dc4be39da7
andrexa1337 <166932134+andrexa1337@users.noreply.github.com>
2024-04-14 17:09:03 +0400

    Add files via upload

---
+++ day6/homework/homework.py
@@ -0,0 +1,3 @@
+print(input("whats your age"))
+age = 12
+age_in_25_years = "age" + (str("25"))
---
+++ day6/homework/hw2.py
@@ -0,0 +1,3 @@
+age = int(input("whats your age: "))
+age_in_25_years = age + 25
+print("users age is " + str(age) + " in 25 years users age will be " + str(age_in_25_years) + " years")
