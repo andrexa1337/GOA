2b843c22b14e8ae7945663e3b80c350db22f6e0e
andrexa1337 <166932134+andrexa1337@users.noreply.github.com>
2024-04-14 17:13:41 +0400

    Add files via upload

---
+++ day17/hw.py
@@ -0,0 +1,15 @@
+# Creating a list with different types of data
+my_list = [10, 'apple', 3.14, True, [1, 2, 3], {'name': 'John', 'age': 30}]
+
+# Displaying the original list
+print("Original list:")
+print(my_list)
+
+# Replacing elements in the list
+my_list[1] = 'banana'  # Replace 'apple' with 'banana'
+my_list[3] = False     # Replace True with False
+my_list[5] = 'Jane'    # Replace the dictionary with a string
+
+# Displaying the modified list
+print("\nModified list:")
+print(my_list)
