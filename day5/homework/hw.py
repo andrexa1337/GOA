9c0e11be2b5850a658b8a296a644e64975b80929
andrexa1337 <166932134+andrexa1337@users.noreply.github.com>
2024-04-14 17:08:33 +0400

    Add files via upload

---
+++ day5/homework/hw.py
@@ -0,0 +1,25 @@
+# Define variables for the ages of family members
+my_age = 12
+mom_age = 32
+dad_age = 32
+sister_age = 9
+
+# Print the ages before adding 5 years
+print("Before adding 5 years:")
+print("My age:", my_age)
+print("Mom's age:", mom_age)
+print("Dad's age:", dad_age)
+print("Sister's age:", sister_age)
+
+# Add 5 years to each age
+my_age += 5
+mom_age += 5
+dad_age += 5
+sister_age += 5
+
+# Print the ages after adding 5 years
+print("\nAfter adding 5 years:")
+print("My age:", my_age)
+print("Mom's age:", mom_age)
+print("Dad's age:", dad_age)
+print("Sister's age:", sister_age)
