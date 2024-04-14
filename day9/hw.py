64fc0ff4a8b6a8377d437abe09c7a455c6e8341d
andrexa1337 <166932134+andrexa1337@users.noreply.github.com>
2024-04-14 17:10:13 +0400

    Add files via upload

---
+++ day9/hw.py
@@ -0,0 +1,20 @@
+# Relative Operators
+x = 5
+y = 10
+
+print("Relative Operators:")
+print("x > y:", x > y)   # False, because 5 is not greater than 10
+print("x < y:", x < y)   # True, because 5 is less than 10
+print("x >= y:", x >= y) # False, because 5 is not greater than or equal to 10
+print("x <= y:", x <= y) # True, because 5 is less than or equal to 10
+print("x == y:", x == y) # False, because 5 is not equal to 10
+print("x != y:", x != y) # True, because 5 is not equal to 10
+
+# Logical Operators
+a = True
+b = False
+
+print("\nLogical Operators:")
+print("a and b:", a and b)   # False, because both a and b are not True
+print("a or b:", a or b)     # True, because a is True
+print("not a:", not a)       # False, because not True is False
