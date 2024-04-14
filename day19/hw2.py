d19441024aff8687588199c4904133e311cbeebe
andrexa1337 <166932134+andrexa1337@users.noreply.github.com>
2024-04-14 17:14:26 +0400

    Add files via upload

---
+++ day19/hw2.py
@@ -0,0 +1,10 @@
+sentence = "Hello, how are you today?"
+
+# Split the sentence into words
+words = sentence.split()
+
+# Print the first word
+print("First word:", words[0])
+
+# Print the last word
+print("Last word:", words[-1])
